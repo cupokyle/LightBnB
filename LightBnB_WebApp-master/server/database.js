7d93af7d21c9bc163de1425f7bf18ab9b1c77fd8
cupokyle <kyleforsythcomedy@gmail.com>
2021-08-19 16:53:43 +0000

    getALLReservations grab from database now functioning

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -64,7 +64,16 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT * FROM reservations
+            JOIN properties ON reservations.property_id = properties.id
+            WHERE reservations.guest_id = $1
+            ORDER BY reservations.start_date DESC
+            LIMIT $2`, [guest_id, limit])
+    .then(result => result.rows)
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getAllReservations = getAllReservations;
 
